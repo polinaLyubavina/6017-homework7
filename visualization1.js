49328cf6754f9eb0e92b9e5440e3422ac8a8d487
polinaLyubavina <polinalyub@tutanota.com>
2022-07-31 16:08:57 -0600

    create async function for await keywords + import wine data

--- visualization1.js
+++ visualization1.js
@@ -1,31 +1,38 @@
 
 //MAP VISUALIZATION
 
-//create SVG
-const width = 800;
-const height = 600;
-const svg = d3.select('#visualization1').attr('width', width).attr('height', height);
+//This function will give an asyncronuous context for the await key words to work
+async function buildVis1() {
+    //create SVG
+    const width = 800;
+    const height = 600;
+    const svg = d3.select('#visualization1').attr('width', width).attr('height', height);
 
-//map projection
-//scale scales it on the page
-//translate can center it on the svg
-const projection = d3.geoEqualEarth().scale(150).translate([width/2, height/2]);
-const path = d3.geoPath(projection);
+    //map projection
+    //scale scales it on the page
+    //translate can center it on the svg
+    const projection = d3.geoEqualEarth().scale(150).translate([width/2, height/2]);
+    const path = d3.geoPath(projection);
 
-//g is a specific svg element that goes into the svg
-//g is what the group is called inside of an svg
-const countries_group = svg.append('g');
+    //g is a specific svg element that goes into the svg
+    //g is what the group is called inside of an svg
+    const countries_group = svg.append('g');
 
-//creates a sequential discrete nine-color scale (using Red-Purple).
-//Given a number t in the range [0,1], returns the corresponding color
-//from the “RdPu” sequential color scheme represented as an RGB string.
-var color_sequential = d3.scaleOrdinal(d3.schemeRdPu[9]);
+    //Import wine data.
+    //Await is a keyword that will make the rest of the code await execution while
+    //the data is loading.
+    const wine_data = await d3.csv('wine-production.csv');
 
-d3.json('countries-110m.json').then((data) => {
+    //creates a sequential discrete nine-color scale (using Red-Purple).
+    //Given a number t in the range [0,1], returns the corresponding color
+    //from the “RdPu” sequential color scheme represented as an RGB string.
+    var color_sequential = d3.scaleOrdinal(d3.schemeRdPu[9]);
+
+    const countries_data = await d3.json('countries-110m.json')
+
     // if you have topojson instead of geojason
-    const countries = topojson.feature(data, data.objects.countries);
-    console.log(countries);
-    
+    const countries = topojson.feature(countries_data, countries_data.objects.countries);
+
     countries_group
         .selectAll('path')
         .data(countries.features)
@@ -42,7 +49,9 @@ d3.json('countries-110m.json').then((data) => {
             d3.select(this).attr('stroke-width', 1);
         })
         ;
-} );
+    ;
+}
 
-
+//calls function
+buildVis1();
 
